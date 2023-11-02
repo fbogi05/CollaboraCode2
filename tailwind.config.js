cc4e56274ae900445a1769373e437ca5b84502e8
Boglarka <boglarkaferencsik05@gmail.com>
2023-11-02 09:58:57 +0100

    base

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/app.component.html",
+    "./src/app/app.component.ts"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
