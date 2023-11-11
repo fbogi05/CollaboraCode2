916924989283606fb7c711fd5b4a31b716cf1757
Boglarka <boglarkaferencsik05@gmail.com>
2023-11-11 10:56:49 +0100

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
