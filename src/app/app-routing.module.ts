8c1e6a7f03f5e2dfc0246e88d05ea321487d7ad4
Boglarka <boglarkaferencsik05@gmail.com>
2024-01-20 17:35:44 +0100

    17:35

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { NavComponent } from './nav/nav.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HomeComponent } from './home/home.component';
+import { HomeAfterComponent } from './home-after/home-after.component';
 
 const routes: Routes = [
   {path:"login", component:LoginComponent},
   {path:"nav", component:NavComponent},
   {path:"sign-up", component:SignUpComponent},
-  {path:"home", component:HomeComponent}
+  {path:"", component:HomeComponent},
+  {path:"home", component:HomeAfterComponent}
 ];
 
 @NgModule({
--- src/app/home-after/home-after.component.ts
+++ src/app/home-after/home-after.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-after.component.css']
 })
 export class HomeAfterComponent {
-
 }
