b35c56d3ad18fb03479d17926388fc6be3e882d7
Boglarka <boglarkaferencsik05@gmail.com>
2024-02-02 18:28:08 +0100

    02021828

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as monaco from 'monaco-editor';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent {
+export class EditorComponent implements AfterViewInit {
+
+  @ViewChild('editorContainer') editorContainer: ElementRef;
+
+  constructor(private elementRef: ElementRef) {
+    this.editorContainer = this.elementRef.nativeElement;
+  }
+
+  ngAfterViewInit() {
+    monaco.editor.create(this.editorContainer.nativeElement, {
+      value: "public Class Elso(){}",
+      language: 'java',
+      theme: 'vs-dark'
+    })
+  }
 
 }
