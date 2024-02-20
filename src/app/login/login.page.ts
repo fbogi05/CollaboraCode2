5b3e38dac22b0017395b0c8ba13023cf98cf6646
Gergo06-py <borbelygergo06@gmail.com>
2024-02-20 22:32:30 +0100

    Authentication without database done

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,33 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BaseService } from '../base.service';
+import { NgForm } from '@angular/forms';
+import { Subscription, fromEvent, map, merge, of } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
+export class LoginPage implements OnInit, OnDestroy {
   fieldData = {
     email: {
-      text: '',
-      pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$',
+      value: '',
+      valid: true,
     },
     password: {
-      text: '',
-      pattern: '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.* ).{8,16}$',
-      hasNumbersPattern: '(?=.*[0-9])',
-      hasLowercasePattern: '(?=.*[a-z])',
-      hasUppercasePattern: '(?=.*[A-Z])',
-      hasSpecialCharsPattern: '(?=.*[\\W])',
-      lengthPattern: '.{8,16}',
+      value: '',
+      valid: true,
       icon: this.base.iconPaths.password.passwordHidden,
       visible: false,
     },
   };
-  titlePosition = 0;
+  networkStatus: boolean = false;
+  networkStatus$: Subscription = Subscription.EMPTY;
 
   constructor(private base: BaseService, private router: Router) {}
+
+  ngOnInit() {
+    window.scrollTo(0, 0);
+    this.checkNetworkStatus();
+  }
+
+  ngOnDestroy() {
+    this.networkStatus$.unsubscribe();
+  }
+
+  checkNetworkStatus() {
+    this.networkStatus = navigator.onLine;
+    this.networkStatus$ = merge(
+      of(null),
+      fromEvent(window, 'online'),
+      fromEvent(window, 'offline')
+    )
+      .pipe(map(() => navigator.onLine))
+      .subscribe((status) => {
+        this.networkStatus = status;
+      });
+  }
 
   changeVisibility() {
     this.fieldData.password.visible = !this.fieldData.password.visible;
@@ -40,7 +60,25 @@ export class LoginPage {
     return this.fieldData.password.visible ? 'text' : 'password';
   }
 
-  login() {
-    this.router.navigate(['/home']);
+  checkCredentials() {
+    let canLogIn = true;
+
+    if (this.fieldData.email.value === 'user@example.com')
+      this.fieldData.email.valid = true;
+    else this.fieldData.email.valid = false;
+    if (this.fieldData.password.value === 'Almafa12;')
+      this.fieldData.password.valid = true;
+    else this.fieldData.password.valid = false;
+
+    canLogIn = this.fieldData.email.valid && this.fieldData.password.valid;
+
+    return canLogIn;
+  }
+
+  login(form: NgForm) {
+    let authenticated = this.checkCredentials();
+    if (form.valid && authenticated) {
+      this.router.navigate(['/home']);
+    }
   }
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,23 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BaseService } from '../base.service';
+import { NgForm } from '@angular/forms';
+import { Subscription, fromEvent, map, merge, of } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage {
+export class RegisterPage implements OnInit, OnDestroy {
   fieldData = {
+    firstName: {
+      value: '',
+      valid: true,
+    },
+    lastName: {
+      value: '',
+      valid: true,
+    },
+    email: {
+      value: '',
+      valid: true,
+      pattern: '^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$',
+    },
     password: {
+      value: '',
+      valid: true,
+      pattern: '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?!.* ).{8,16}$',
+      hasNumbersPattern: '(?=.*[0-9])',
+      hasLowercasePattern: '(?=.*[a-z])',
+      hasUppercasePattern: '(?=.*[A-Z])',
+      lengthPattern: '^.{8,16}$',
       icon: this.base.iconPaths.password.passwordHidden,
       visible: false,
     },
     passwordAgain: {
+      value: '',
+      valid: true,
       icon: this.base.iconPaths.password.passwordHidden,
       visible: false,
     },
   };
+  networkStatus: boolean = false;
+  networkStatus$: Subscription = Subscription.EMPTY;
+
+  ngOnInit() {
+    window.scrollTo(0, 0);
+    this.checkNetworkStatus();
+  }
+
+  ngOnDestroy() {
+    this.networkStatus$.unsubscribe();
+  }
+
+  checkNetworkStatus() {
+    this.networkStatus = navigator.onLine;
+    this.networkStatus$ = merge(
+      of(null),
+      fromEvent(window, 'online'),
+      fromEvent(window, 'offline')
+    )
+      .pipe(map(() => navigator.onLine))
+      .subscribe((status) => {
+        this.networkStatus = status;
+      });
+  }
 
   constructor(private base: BaseService, private router: Router) {}
 
@@ -28,7 +76,8 @@ export class RegisterPage {
         this.fieldData.password.visible
       );
     } else if (field === 'passwordAgain') {
-      this.fieldData.passwordAgain.visible = !this.fieldData.passwordAgain.visible;
+      this.fieldData.passwordAgain.visible =
+        !this.fieldData.passwordAgain.visible;
       this.fieldData.passwordAgain.icon = this.base.getPasswordIcon(
         this.fieldData.passwordAgain.visible
       );
@@ -43,7 +92,37 @@ export class RegisterPage {
     else return 'text';
   }
 
-  register() {
-    this.router.navigate(['/home']);
+  validateData() {
+    if (this.fieldData.firstName.value !== '')
+      this.fieldData.firstName.valid = true;
+    else this.fieldData.firstName.valid = false;
+    if (this.fieldData.lastName.value !== '')
+      this.fieldData.lastName.valid = true;
+    else this.fieldData.lastName.valid = false;
+    if (
+      this.fieldData.email.value !== '' &&
+      this.fieldData.email.value.match(this.fieldData.email.pattern)
+    )
+      this.fieldData.email.valid = true;
+    else this.fieldData.email.valid = false;
+    if (
+      this.fieldData.password.value !== '' &&
+      this.fieldData.passwordAgain.value.match(this.fieldData.password.pattern)
+    )
+      this.fieldData.password.valid = true;
+    else this.fieldData.password.valid = false;
+    if (
+      this.fieldData.passwordAgain.value !== '' &&
+      this.fieldData.passwordAgain.value === this.fieldData.password.value
+    )
+      this.fieldData.passwordAgain.valid = true;
+    else this.fieldData.passwordAgain.valid = false;
+  }
+
+  register(form: NgForm) {
+    this.validateData();
+    if (form.valid) {
+      this.router.navigate(['/home']);
+    }
   }
 }
