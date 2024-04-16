84067608f354cf75fccd08fee2528a74f04f92cd
Gergo06-py <borbelygergo06@gmail.com>
2024-04-16 17:01:25 +0200

    Changed problem with last edit time on file card not showing properly and updated developer information on about page

--- src/app/file-card/file-card.component.ts
+++ src/app/file-card/file-card.component.ts
@@ -23,12 +23,17 @@ export class FileCardComponent implements OnInit {
       let editedTime = new Date(
         this.file.lastEditInformation.lastEditedTime
       ).getTime();
+      let editedDate = new Date(this.file.lastEditInformation.lastEditedTime);
+      let currentDate = new Date();
+      let daysSinceLastEdit = this.daysBetween(editedDate, currentDate);
+      console.log(daysSinceLastEdit);
+
       let currentTime = new Date().getTime();
       let lastEditTime = currentTime - editedTime;
       let hours = Math.floor((lastEditTime / 1000 / 60 / 60) % 24);
       let minutes = Math.floor((lastEditTime / 1000 / 60) % 60);
       let seconds = Math.floor((lastEditTime / 1000) % 60);
-      if (hours < 24) {
+      if (daysSinceLastEdit < 1) {
         return `${hours > 0 ? hours + ' órával' : ''}${
           hours > 0 && minutes > 0 ? ', ' : ''
         }${minutes > 0 ? minutes + ' perccel' : ''}${
@@ -37,9 +42,13 @@ export class FileCardComponent implements OnInit {
           minutes < 1 && seconds > 0 ? seconds + ' másodperccel' : ''
         } ezelőtt`;
       } else {
-        return this.datePipe.transform(
-          this.file.lastEditInformation.lastEditedTime
-        );
+        if (daysSinceLastEdit < 30) {
+          return `${daysSinceLastEdit} nappal ezelőtt`;
+        } else {
+          return this.datePipe.transform(
+            this.file.lastEditInformation.lastEditedTime
+          );
+        }
       }
     }
   };
@@ -60,5 +69,22 @@ export class FileCardComponent implements OnInit {
     } else {
       document.documentElement.classList.remove('dark');
     }
+  }
+
+  daysBetween(first: Date, second: Date) {
+    // Copy date parts of the timestamps, discarding the time parts.
+    let one = new Date(first.getFullYear(), first.getMonth(), first.getDate());
+    let two = new Date(
+      second.getFullYear(),
+      second.getMonth(),
+      second.getDate()
+    );
+
+    // Do the math.
+    let millisecondsPerDay = 1000 * 60 * 60 * 24;
+    let millisBetween = two.getTime() - one.getTime();
+    let days = millisBetween / millisecondsPerDay;
+
+    return days;
   }
 }
